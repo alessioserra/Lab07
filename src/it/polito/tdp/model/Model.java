4e58ccc4eb9022b99df9e2cf06758754a7de8d79
zierp <zierp@DESKTOP-RURJMSA.homenet.telecomitalia.it>
2019-04-17 09:38:27 +0200

    v 1.1

--- src/it/polito/tdp/model/Model.java
+++ src/it/polito/tdp/model/Model.java
@@ -1,5 +1,19 @@
 package it.polito.tdp.model;
 
+import java.util.List;
+
+import it.polito.tdp.dao.PowerOutageDAO;
+
 public class Model {
+
+	PowerOutageDAO podao;
+	
+	public Model() {
+		podao = new PowerOutageDAO();
+	}
+	
+	public List<Nerc> getNercList() {
+		return podao.getNercList();
+	}
 
 }
